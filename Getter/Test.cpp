acef1c990776e3be420a578116b213bed73cfc16
Soskar1 <oskar.vistorskis@gmail.com>
2023-05-02 14:51:58 +0300

    speed tests

--- Getter/Test.cpp
+++ Getter/Test.cpp
@@ -1,12 +1,134 @@
 #include <iostream>
 #include <vector>
 #include <ctime>
+#include <string>
 
 #include "Getter.h"
 #include "Data.h"
+#include "Random.h"
+#include "Experimentator.h"
+
+#define ROWS 50
+#define COLUMNS 50
+
+#define STRING_LENGTH 5
+#define DOUBLE_PRECISION 3
+
+Data* LinearSearchByInt(std::vector<std::vector<Data>>& data, const int& value) {
+	for (int i = 0; i < data.size(); ++i) {
+		for (int j = 0; j < data[i].size(); ++j) {
+			if (data[i][j].intField == value)
+				return &data[i][j];
+		}
+	}
+
+	return nullptr;
+}
+
+Data* LinearSearchByDouble(std::vector<std::vector<Data>>& data, const double& value) {
+	for (int i = 0; i < data.size(); ++i) {
+		for (int j = 0; j < data[i].size(); ++j) {
+			if (data[i][j].doubleField == value)
+				return &data[i][j];
+		}
+	}
+
+	return nullptr;
+}
+
+Data* LinearSearchByString(std::vector<std::vector<Data>>& data, const std::string& value) {
+	for (int i = 0; i < data.size(); ++i) {
+		for (int j = 0; j < data[i].size(); ++j) {
+			if (data[i][j].stringField == value)
+				return &data[i][j];
+		}
+	}
+
+	return nullptr;
+}
 
 int main() {
+	srand((unsigned)time(NULL));
 
+	std::vector<std::vector<Data>> data;
+	Getter::TreeGetter treeGetter(data);
+	
+	for (int i = 0; i < ROWS; ++i) {
+	
+	    std::vector<Data> tmp;
+	    for (int j = 0; j < COLUMNS; ++j) {
+	        Data newData;
+	        newData.intField = rand();
+	        newData.doubleField = Random::DBLRand(DOUBLE_PRECISION);
+	        newData.stringField = Random::STRRand(STRING_LENGTH);
+	
+	        tmp.push_back(newData);
+	    }
+	
+	    data.push_back(tmp);
+	}
+
+	double executionTime = 0;
+
+	executionTime = GetAverageExecutionTime([&data]() {
+		for (int i = 0; i < data.size(); ++i) {
+			for (int j = 0; j < data[i].size(); ++j) {
+				LinearSearchByInt(data, data[i][j].intField);
+			}
+		}
+		});
+
+	std::cout << "LinearSearch/int/" << ROWS << "rows/" << COLUMNS << "columns average execution time: " << executionTime / (ROWS * COLUMNS) << " microseconds\n";
+
+	executionTime = GetAverageExecutionTime([&data]() {
+		for (int i = 0; i < data.size(); ++i) {
+			for (int j = 0; j < data[i].size(); ++j) {
+				LinearSearchByDouble(data, data[i][j].doubleField);
+			}
+		}
+		});
+
+	std::cout << "LinearSearch/double/" << ROWS << "rows/" << COLUMNS << "columns average execution time: " << executionTime / (ROWS * COLUMNS) << " microseconds\n";
+
+	executionTime = GetAverageExecutionTime([&data]() {
+		for (int i = 0; i < data.size(); ++i) {
+			for (int j = 0; j < data[i].size(); ++j) {
+				LinearSearchByString(data, data[i][j].stringField);
+			}
+		}
+		});
+
+	std::cout << "LinearSearch/string/" << ROWS << "rows/" << COLUMNS << "columns average execution time: " << executionTime / (ROWS * COLUMNS) << " microseconds\n";
+
+	executionTime = GetAverageExecutionTime([data, treeGetter]() {
+		for (int i = 0; i < data.size(); ++i) {
+			for (int j = 0; j < data[i].size(); ++j) {
+				treeGetter.SearchByInt(data[i][j].intField);
+			}
+		}
+		});
+
+	std::cout << "TreeGetter/int/" << ROWS << "rows/" << COLUMNS << "columns average execution time: " << executionTime / (ROWS * COLUMNS) << " microseconds\n";
+
+	executionTime = GetAverageExecutionTime([data, treeGetter]() {
+		for (int i = 0; i < data.size(); ++i) {
+			for (int j = 0; j < data[i].size(); ++j) {
+				treeGetter.SearchByDouble(data[i][j].doubleField);
+			}
+		}
+		});
+
+	std::cout << "TreeGetter/double/" << ROWS << "rows/" << COLUMNS << "columns average execution time: " << executionTime / (ROWS * COLUMNS) << " microseconds\n";
+
+	executionTime = GetAverageExecutionTime([data, treeGetter]() {
+		for (int i = 0; i < data.size(); ++i) {
+			for (int j = 0; j < data[i].size(); ++j) {
+				treeGetter.SearchByString(data[i][j].stringField);
+			}
+		}
+		});
+
+	std::cout << "TreeGetter/string/" << ROWS << "rows/" << COLUMNS << "columns average execution time: " << executionTime / (ROWS * COLUMNS) << " microseconds\n";
 
 	return 0;
 }
